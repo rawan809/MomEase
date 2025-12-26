290b625f9f0ae171da5a6aa3070f2478aa8d487e
unknown <rawanghoniama@gmail.com>
2025-12-26 20:22:09 +0200

    merged landing page

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,9 @@ import LandingPage from "../pages/LandingPage";
 function App() {
   return (
     <>
-      
-      <div className="p-10">
-        <p className="text-primary">primary</p>
-        <p className="text-muted bg-background">muted</p>
-        <button className="shadow-md bg-accent text-primary px-5 radius-lg rounded-lg">
-          btn
-        </button>
-        <p className="text-primary font-brand text-2xl">MomEase</p>
-      </div>
-      <LandingPage />
+      <MainLayout>
+        <LandingPage />
+      </MainLayout>
     </>
   );
 }
