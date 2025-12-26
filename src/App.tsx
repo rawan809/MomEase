357486c3e99395dc18aec0c2e40f827f4b8f9cf5
unknown <rawanghoniama@gmail.com>
2025-12-26 12:21:59 +0200

    refactor: initial project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
-
   return (
     <>
-     </>
-  )
+      <div className="p-10">
+        <p className="text-primary">primary</p>
+        <p className="text-muted bg-background">muted</p>
+        <button className="shadow-md bg-accent text-primary px-5 radius-lg rounded-lg">btn</button>
+        <p className="text-primary font-brand text-2xl">MomEase</p>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
