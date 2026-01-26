d6b33b3e24d5931a8aaaf7614e10bef742642a7b
unknown <rawanghoniama@gmail.com>
2026-01-26 23:46:00 +0200

    Not found page edit

--- pages/Notfound.tsx
+++ pages/Notfound.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function Notfound() {
   return (
-    <div className="flex h-screen flex-col justify-center items-center gap-4">
+    <div className="flex h-screen flex-col justify-center items-center gap-4 p-5 text-center">
       <p className="text-xl"><span className="text-primary text-h2 pr-1 font-bold">404</span>Page Not Found</p>
       <p className="text-muted">The page you're looking for doesn't exist or has been moved.</p>
       <Link className="bg-accent rounded-lg p-3 shadow-md" to={"/"}>Go Home</Link>
