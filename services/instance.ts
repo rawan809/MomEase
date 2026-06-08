6020eda1b7c8df7431b4eaa564a433d76c356972
unknown <rawanghoniama@gmail.com>
2026-06-08 23:44:07 +0300

    netlify _redirects

--- services/instance.ts
+++ services/instance.ts
@@ -2,9 +2,11 @@ import axios from "axios";
 import i18n from "@/i18n";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "/api",
+  baseURL:
+    // import.meta.env.VITE_API_URL ??
+    "/api",
+  // withCredentials: false,
 });
-
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
